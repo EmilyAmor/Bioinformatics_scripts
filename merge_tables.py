706a8f6323c99ebe3fe0be47805afa7ddc0bbeaa
EmilyAmor <emily.amor.stander@gmail.com>
2018-09-13 13:34:43 +0200

    script for merging transcriptome annotation dataframes

---
+++ merge_tables.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import click
+import pandas as pd
+
+@click.command()
+@click.argument('table_1')
+@click.argument('table_1_separator')
+@click.argument('table_2')
+@click.argument('table_2_separator')
+@click.argument('new_file_name')
+
+def merge_annotation_tables(table_1, table_1_separator, table_2, table_2_separator, new_file_name):
+    """Merges two transcriptome annotation tables (on 'transcripts' column), given the tables and their separators as input. Ouputs table in csv format"""
+    engine='python'
+    t1 = pd.read_table(table_1,sep=table_1_separator, engine='python')
+    t2 = pd.read_table(table_2,sep=table_2_separator, engine='python')
+    merge = pd.merge(t1, t2, on='transcript', how="left")
+    merge.to_csv(new_file_name)
+
+
+if __name__ == '__main__':
+      merge_annotation_tables()
