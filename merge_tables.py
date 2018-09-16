a537b1bc5306ec7af9d45caaefab7064bbde8748
EmilyAmor <emily.amor.stander@gmail.com>
2018-09-16 12:45:22 +0200

    merge tables now outputs a tsv instead of csv, and updated taxonomy dictionary in kraken classification script

--- merge_tables.py
+++ merge_tables.py
@@ -11,11 +11,11 @@ import pandas as pd
 @click.argument('new_file_name')
 
 def merge_annotation_tables(table_1, table_1_separator, table_2, table_2_separator, new_file_name):
-    """Merges two transcriptome annotation tables (on 'transcripts' column), given the tables and their separators as input. Ouputs table in csv format"""
+    """Merges two transcriptome annotation tables (on 'transcripts' column), given the tables and their separators as input. Ouputs table in tsv format"""
     t1 = pd.read_table(table_1,sep=table_1_separator, engine='python')
     t2 = pd.read_table(table_2,sep=table_2_separator, engine='python')
-    merge = pd.merge(t1, t2, on='transcript', how="left")
-    merge.to_csv(new_file_name)
+    merge = pd.merge(t1, t2, how="left", on="transcript")
+    merge.to_csv(new_file_name, sep='\t')
 
 
 if __name__ == '__main__':
