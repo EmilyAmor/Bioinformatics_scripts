c0eb7e18f4a06cf37603aad5ba4679e9a032540c
EmilyAmor <emily.amor.stander@gmail.com>
2018-10-17 12:25:10 +0200

    script to calculate number of reads that map to busco contigs

---
+++ reads_mapped_to_busco_contigs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import click
+import pandas as pd
+from Bio import SeqIO
+@click.command()
+@click.argument('contigs_hit_busco')
+@click.argument('bowtie_stats')
+
+def mapped_reads_to_busco_contigs(contigs_hit_busco, bowtie_stats ):
+
+    # read in list of contigs that have a hit to a busco
+    file = open(contigs_hit_busco, 'r')
+    busco_transcripts = []
+    for line in file:
+        busco_transcripts.append(line.strip())
+
+    # read in mapped_reads_to_contig.tsv file from bowtie2 .sam output
+    reads_that_map = pd.read_csv(bowtie_stats, sep='\t', header=0)
+    mapped_reads_dict = dict(zip(reads_that_map['transcripts'], reads_that_map['mapped_reads']))
+
+    new_dict = {}
+    for transcript in busco_transcripts:
+        new_dict[transcript] = mapped_reads_dict[transcript]
+
+    s = pd.Series(new_dict)
+    s.to_csv('mapped_reads_to_busco_contigs.tsv', sep='\t')
+
+if __name__ == '__main__':
+    mapped_reads_to_busco_contigs()
