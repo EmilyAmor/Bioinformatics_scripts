b44104d388076c42bd59a3b28ea7710fa408743b
EmilyAmor <emily.amor.stander@gmail.com>
2019-01-10 09:42:33 +0200

    add better specifications on the format of the input file

--- parse_diamond_taxonomy_count.py
+++ parse_diamond_taxonomy_count.py
@@ -6,15 +6,20 @@ import numpy as np
 import sys
 
 
-def parse_diamond_taxonomy_count(input_new_filename_tsv, taxon_level, output_file_name_tsv):
-    """ Read in tsv in format "transcript","kingdom", "phylum","class","order","family" and gives stipulated counts"""
+def parse_diamond_taxonomy_count(input_new_filename_tsv, output_file_name_tsv):
+    """ Read in tsv in format "transcript","kingdom", "phylum","class","order","family, genus, specie" and gives stipulated counts"""
     df = pd.read_csv(input_new_filename_tsv, sep='\t')
-    new_df = df.set_index(["transcript","kingdom", "phylum","class","order","family"]).count(level=taxon_level)
-    new_df.to_csv(output_file_name_tsv, sep='\t')
+    taxonomy = ["kingdom", "phylum", "class", "order", "family", "g", "specie"]
+    appended_data = []
+    df_all = pd.DataFrame()
+    for i in taxonomy:
+        data = df[i].value_counts().reset_index()
+        appended_data.append(data)
+    appended_data = pd.concat(appended_data, axis=1)
+    appended_data.to_csv(output_file_name_tsv, sep="\t")
 
 
 if __name__ == '__main__':
     input_new_filename_tsv = sys.argv[1]
-    taxon_level = sys.argv[2]
-    output_file_name_tsv = sys.argv[3]
-    parse_diamond_taxonomy_count(input_new_filename_tsv, taxon_level, output_file_name_tsv)
+    output_file_name_tsv = sys.argv[2]
+    parse_diamond_taxonomy_count(input_new_filename_tsv, output_file_name_tsv)
